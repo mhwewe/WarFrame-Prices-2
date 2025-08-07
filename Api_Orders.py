fdf2d2721a92029a6e734882ab158ec89c563edb
MHwewe <hrkhrg54@gmail.com>
2025-08-07 04:07:28 +0330

    s

--- Api_Orders.py
+++ Api_Orders.py
@@ -34,7 +34,7 @@ def orders(item_link):
         orders_list = sorted(orders_list, key=itemgetter('platinum'))
         buy_list: list = []
         sell_list: list = []
-        bruh = ''
+        bruh = 's'
 
         for i in orders_list:
             if i['order_type'] == 'buy':
